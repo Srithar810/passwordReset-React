a576782a3e91909b136216dca2a2ec6d3cbc44ff
Srithar810 <srithar.s810@gmail.com>
2025-08-20 10:39:19 +0530

    body update

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import NavBar from "./NavBar";
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
+import Register from "./Register";
 
 const Body = () => {
   return (
     <div>
       <NavBar />
+      <Register></Register>
       <Outlet />
       <Footer />
     </div>
